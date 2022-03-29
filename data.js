0ed7bcf0052f08df2c12593b74bbba2f6bfb8451
Jaziel Rodriguez <aguafapolla@gmail.com>
2022-03-28 20:57:21 -0600

    iterar restaurantes y actividades

---
+++ data.js
@@ -0,0 +1,20 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const uri =
+  'mongodb+srv://admin-your-hotels:polapolA1@cluster0.9yvah.mongodb.net/your-hotels?retryWrites=true&w=majority'
+let database
+async function connect () {
+  const client = await MongoClient.connect(uri)
+  database = client.db('your-hotels')
+}
+function getDb () {
+  if (!database) {
+    throw { error: 'No database found' }
+  }
+  return database
+}
+module.exports = {
+  getDb,
+  connect
+}
--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express')
+const db = require('./data.js')
 const app = express()
 const port = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.get('/rooms', (req, res) => {
-  res.render('rooms')
+app.get('/rooms', async (req, res) => {
+  const rooms = await db.getDb().collection('rooms').find().toArray()
+  res.render('rooms', { rooms })
 })
-app.get('/restaurants', (req, res) => {
+app.get('/restaurants', async (req, res) => {
+  const restaurants = await db.get().collection('restaurants').find().toArray()
+  // await console.log(restaurants)
   res.render('restaurants')
 })
-app.get('/activities', (req, res) => {
+app.get('/activities', async (req, res) => {
+  const activities = await db.get().collection('activities').find().toArray()
+  // await console.log(activities)
   res.render('activities')
 })
 app.get('/cart', (req, res) => {
   res.render('cart')
 })
-app.listen(port)
+db.connect().then(() => {
+  app.listen(port)
+})
