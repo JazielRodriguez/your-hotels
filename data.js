0c614384816dba28f4f74e8780209286af075a88
Jaziel Rodriguez <aguafapolla@gmail.com>
2022-04-01 21:06:07 -0600

    check the images

--- data.js
+++ data.js
@@ -1,8 +1,6 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
-
-const uri =
-  'mongodb+srv://admin-your-hotels:polapolA1@cluster0.9yvah.mongodb.net/your-hotels?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://admin-your-hotels:polapolA1@cluster0.9yvah.mongodb.net/your-hotels?retryWrites=true&w=majority'
 let database
 async function connect () {
   const client = await MongoClient.connect(uri)
